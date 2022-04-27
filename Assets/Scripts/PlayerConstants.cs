a75ad1793781fb120e29fadee2a2129bbb551eb7
jdavalos1 <43660774+jdavalos1@users.noreply.github.com>
2022-04-26 17:12:34 -0700

    Revert "Jd create basic movement"

--- Assets/Scripts/PlayerConstants.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public static class PlayerConstants
-{
-    public static string HorizontalAxis = "Horizontal";
-    public static string VerticalAxis = "Vertical";
-}
