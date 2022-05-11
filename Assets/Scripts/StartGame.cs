323a2a0e574d76c8b8a1c9efd075f965c6128fa0
Karenmart91 <k.martinez9108@yahoo.com>
2022-05-11 13:18:22 -0700

    Update_UIs

--- Assets/Scripts/StartGame.cs
+++ Assets/Scripts/StartGame.cs
@@ -1,18 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class StartGame : MonoBehaviour
+public class StartGameUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void StartGame(){ 
+        Debug.Log ("Game Has Started");
+        SceneManager.LoadScene("Theseus");
+            
     }
 }
