1ddad032dd0bd78762748235457f791a42f1ac42
Juan Davalos <jcharlied13@gmail.com>
2022-05-12 00:00:45 -0700

    removed the call to open door when quiz launches

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -20,7 +20,6 @@ public class GameManager : MonoBehaviour
     {
         if (currentDoor == null) currentDoor = d;
         player.CanMove = false;
-        currentDoor.ContinueToNextSection();
     }
 
     public void PassedQuiz()
