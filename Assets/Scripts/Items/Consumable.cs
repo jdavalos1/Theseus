3a165f212fd9467a32caf33f7f1af3ee7a7393c7
Juan Davalos <jcharlied13@gmail.com>
2022-05-14 12:49:59 -0700

    getting ready to set up the sprite viewing system

--- Assets/Scripts/Items/Consumable.cs
+++ Assets/Scripts/Items/Consumable.cs
@@ -1,10 +1,13 @@
 using System;
+using UnityEngine;
 
 [Serializable]
 public abstract class Consumable
 {
     public string Title;
     public string Description;
+    [NonSerialized]
+    public Sprite sprite;
 
     public abstract void UseItem(Player p);
 }
